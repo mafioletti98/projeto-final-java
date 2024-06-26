6510b8b3e02913893d5824884526f95333053f03
Fernanda costa <fernandinhacosta.97@hotmail.com>
2024-06-26 11:56:00 -0300

    adicionando a classe batalha de definindo regras das batalhas

---
+++ src/Sistema/Batalha.java
@@ -0,0 +1,10 @@
+package Sistema;
+
+public class Batalha {
+    // selecionar deck pra batalha.
+    // decks temq ue ter a mesma quantidade de cartas pra jogar.
+    // math. randon pra selecionar uma carta
+    // seleciona uma carta de um e uma carta de outro e compara
+    // perder 50 de vida de jogador ou 5 raunds e ganhar quem tem mais vida
+
+}
