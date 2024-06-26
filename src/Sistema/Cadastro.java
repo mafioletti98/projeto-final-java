af818f178e3399bffdd816c58e4c9d048d11bff6
Atomossss <victorxdobro@gmail.com>
2024-06-26 15:53:35 -0300

    Exceção com try catch implementado no arquivo Cadastro.java nas linhas 9-34

--- src/Sistema/Cadastro.java
+++ src/Sistema/Cadastro.java
@@ -6,22 +6,33 @@ import Models.Deck;
 
 public class Cadastro {
 
- 
-//cadastrar jogador, com exceção caso o jogador  cadastre nome com caracteres especiais
-public static Jogador cadastrarJogador() throws Exception {
-    Jogador jogadorTemporario = new Jogador();
-    System.out.println("Por favor, digite o nome de usuário: ");
-    String nome = Console.lerString();
-
-    // Validação de nome com caracteres especiais (regex)
-    if (!nome.matches("[\\p{L}\\p{N}\\p{Space}][\\p{L}\\p{N}\\p{Space}]*")) {
-        throw new Exception("\nErro: O nome do jogador não pode conter caracteres especiais!");
+    public static Jogador cadastrarJogador() throws Exception {
+        Jogador jogadorTemporario = null; // Inicialize o jogador como null
+        boolean nomeValido = false; // Flag para controlar a validação
+    
+        while (!nomeValido) { // Loop while para repetir a entrada do nome
+            try {
+                System.out.println("Por favor, digite o nome de usuário: ");
+                String nome = Console.lerString();
+    
+                // Validação de nome com caracteres especiais (regex)
+                if (!nome.matches("[\\p{L}\\p{N}\\p{Space}][\\p{L}\\p{N}\\p{Space}]*")) {
+                    throw new Exception("\nErro: O nome do jogador não pode conter caracteres especiais!");
+                }
+    
+                jogadorTemporario = new Jogador(); // Crie o jogador apenas se o nome for válido
+                jogadorTemporario.setNome(nome);
+                nomeValido = true; // Defina a flag como true para sair do loop
+                System.out.println("Jogador cadastrado com sucesso!");
+    
+            } catch (Exception e) {
+                System.out.println(e.getMessage()); // Exiba a mensagem de erro
+            }
+        }
+    
+        return jogadorTemporario; // Retorne o jogador após a validação
     }
-
-    jogadorTemporario.setNome(nome);
-    System.out.println("Jogador cadastrado com sucesso!");
-    return jogadorTemporario;
-}
+    
 
 
 public static Deck cadastrarNovoDeck() throws Exception {
